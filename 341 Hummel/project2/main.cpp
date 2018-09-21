ad18f7b2c127aa45f19f5ec3ed61a338b52a6aa3
Affan Farid <affanfarid29@gmail.com>
2018-09-21 15:07:54 -0500

    341 proj2 started

---
+++ 341 Hummel/project2/main.cpp
@@ -0,0 +1,53 @@
+/*main.cpp*/
+
+//
+// Netflix movie review analysis.
+//
+// Affan Farid
+// UIN 671466593
+// U. of Illinois, Chicago
+// CS 341: Fall 2018
+// Project 02
+//
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <chrono>
+
+using namespace std;
+
+
+int main()
+{
+  cout << "** Netflix Movie Review Analysis **" << endl;
+  cout << endl;
+
+  //
+  // input the filenames to process:
+  //
+  string moviesFN, reviewsFN;
+
+  cin >> moviesFN;
+  cin >> reviewsFN;
+
+  cout << moviesFN << endl;
+  cout << reviewsFN << endl;
+  cout << endl;
+
+  //
+  // TODO:
+  //
+
+
+  //
+  // done:
+  //
+  cout << "** DONE! **" << endl << endl;
+
+  return 0;
+}
