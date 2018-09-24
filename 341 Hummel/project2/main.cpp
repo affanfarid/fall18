faa71c7b949d28804d68c21a07b642d1f74419ea
Affan Farid <affanfarid29@gmail.com>
2018-09-24 12:43:31 -0500

    finished, no comments

--- 341 Hummel/project2/main.cpp
+++ 341 Hummel/project2/main.cpp
@@ -24,7 +24,6 @@ using namespace std;
 class Movie;
 class Review;
 
-
 class Review{
   private:
 
@@ -64,9 +63,13 @@ class Movie{
   int numReviews;
   double averageRating = 0.0;
 
+  int starFrequencies[6] = {-1,0,0,0,0,0};
+
   vector<Review> movieReviews;
 
   public:
+
+  Movie();
 
   Movie(int id, string name, int year){
     movieID = id;
@@ -88,14 +91,21 @@ class Movie{
 
     averageRating = double(averageRating)/double(numReviews);
 
+    //makes sure averageRating is not NaN
+    if(averageRating != averageRating){
+      averageRating = 0;
+    }
+
     return;
   }
-
-  void printMovie(int &i){
-    cout<<i+1<<".\t"<<movieID<<"\t"<<numReviews<<
-    "\t"<<averageRating<<"\t'"<<movieName<<"'"<<endl;
+  void calculateFrequencies(){
+    starFrequencies[0] = -1;
+    for(auto &r: movieReviews){
+      starFrequencies[r.getRating()]++;
+    }
   }
 
+  int getNumStars(int starnum){return starFrequencies[starnum];}
   int getMovieID(){return movieID;}
   string getMovieName(){return movieName;}
   int getMovieYear(){return yearPublished;}
@@ -197,71 +207,91 @@ void printTop10Movies(vector<Movie> &movies, vector<Review> &reviews){
 }
 
 //TODO
-void printReview(Review &r){
-  //TODO: add name
-  cout<<"Movie: "<<r.getMovieID() <<"\t("<<r.getMovieID()<<")"<<endl;
+void printReview(Review &r, vector<Movie> movies){
+
+  string movieName;
+
+  for(auto &m: movies){
+    if(r.getMovieID() == m.getMovieID()){
+      movieName = m.getMovieName();
+      break;
+    }
+  }
+
+  cout<<"Movie: "<<r.getMovieID() <<"\t("<<movieName<<")"<<endl;
   cout<<"Num stars:\t"<<r.getRating()<<endl;;
   cout<<"User id:\t"<<r.getUserID()<<endl;
   cout<<"Date:\t\t"<<r.getReviewDate()<<endl;
 
 }
-//TODO
+
 void printMovie(Movie & m){
+  //calculates the number of each star from all the movie's reviews
+  m.calculateFrequencies();
+
+  //prints info
   cout<<"Movie:\t\t"<<m.getMovieName()<<endl;
   cout<<"Year:\t\t"<<m.getMovieYear()<<endl;
   cout<<"Avg rating:\t"<<m.getMovieRating()<<endl;
   cout<<"Num reviews:\t"<<m.getNumReviews()<<endl;
-  cout<<" 1 star:\t"<<endl;
-  cout<<" 2 star:\t"<<endl;
-  cout<<" 3 star:\t"<<endl;
-  cout<<" 4 star:\t"<<endl;
-  cout<<" 5 star:\t"<<endl;
 
+  //prints the star lines
+  int i = 1;
+  cout<<" "<<i<<" star:\t"<<m.getNumStars(i)<<endl;
+  for(int i =2; i<=5; i++){
+    cout<<" "<<i<<" stars:\t"<<m.getNumStars(i)<<endl;
+  }
 }
 
 void movieReviewInfo(vector<Movie> &movies, vector<Review> &reviews){
   string input;
   int num;
+  bool found;
 
   cout<<endl<<">> Movie and Review Information <<"<<endl<<endl;
 
   while(true){
+    found = false;
     cout<<"Please enter a movie ID (< 100,000), a review ID (>= 100,000), 0 to stop> ";
     cin>>input;
     num = stoi(input);
-
     cout<<endl;
+
+    
     if(num == 0) return;
+    else if(num < 0) {
+      cout<<"**invalid id..."<<endl<<endl; continue;
+    }
     else if(num>100000){
       //searches reviews
-
       for(auto &r: reviews){
         if(r.getReviewID() == num){
-          printReview(r);
+          printReview(r,movies);
+          found = true;
           break;
         }
       }
       //TODO
-      cout<<"review not found..."<<endl<<endl;
+      if (!found)cout<<"review not found..."<<endl<<endl;
       continue;
 
     }
     else {
       //searches movies
-
       for(auto &m: movies){
         if(m.getMovieID() == num){
+          m.calcuateAvgReview(reviews);
           printMovie(m);
+          found = true;
           break;
         }
       }
       //TODO
-      cout<<"movie not found..."<<endl<<endl;
+      if(!found)cout<<"movie not found..."<<endl<<endl;
       continue;
     }
 
   }
-
 
 }
 
